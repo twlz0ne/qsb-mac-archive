33e5480bd985fe37aecdedada3a84193c9d4f0d4
qsbmac.daemon <qsbmac.daemon@c7262d1c-4ebb-4cc0-bccf-18c293255191>
2009-03-30 21:45:23 +0000

    [Author: hawk]
    Test for the Python search source
    
    OCL=10637799
    DELTA=101 (100 added, 0 deleted, 1 changed)
    R=dmaclach
    SCL=10652533
    
    
    git-svn-id: file:///Users/gqj/repos/qsb-mac-archive/p/trunk@134 c7262d1c-4ebb-4cc0-bccf-18c293255191

--- Vermilion/Vermilion/HGSPythonSourceTest.m
+++ Vermilion/Vermilion/HGSPythonSourceTest.m
@@ -33,9 +33,109 @@
 
 #import "GTMSenTestCase.h"
 #import "HGSPythonSource.h"
+#import "HGSQuery.h"
+#import "HGSResult.h"
 
-@interface HGSPythonSourceTest : GTMTestCase 
+@interface HGSPythonSourceTest : GTMTestCase {
+  NSArray *results_;
+}
 @end
 
 @implementation HGSPythonSourceTest
+
+- (void)setUp {
+  NSNotificationCenter *nc = [NSNotificationCenter defaultCenter];
+  [nc addObserver:self
+         selector:@selector(gotResults:)
+             name:kHGSSearchOperationDidUpdateResultsNotification
+           object:nil];
+}
+
+- (void)tearDown {
+  NSNotificationCenter *nc = [NSNotificationCenter defaultCenter];
+  [nc removeObserver:self];
+  [results_ release];
+}
+
+- (void)testSource {
+  HGSPython *sharedPython = [HGSPython sharedPython];
+  STAssertNotNil(sharedPython, nil);
+  
+  NSBundle *bundle = [NSBundle bundleForClass:[self class]];
+  [sharedPython appendPythonPath:[bundle resourcePath]];
+  
+  NSDictionary *config = [NSDictionary dictionaryWithObjectsAndKeys:
+                          @"VermilionTest", kPythonModuleNameKey,
+                          @"VermilionTest", kPythonClassNameKey,
+                          @"python.test", kHGSExtensionIdentifierKey,
+                          bundle, kHGSExtensionBundleKey,
+                          nil];
+  STAssertNotNil(config, nil);
+
+  HGSPythonSource *source
+    = [[[HGSPythonSource alloc] initWithConfiguration:config] autorelease];
+  STAssertNotNil(source, nil);
+
+  HGSQuery *query = [[[HGSQuery alloc] initWithString:@"Hello world"
+                                              results:nil
+                                           queryFlags:0] autorelease];
+  STAssertNotNil(query, nil);
+  
+  STAssertTrue([source isValidSourceForQuery:query], nil);
+  
+  HGSPythonSearchOperation *op = [[[HGSPythonSearchOperation alloc]
+                                   initWithQuery:query
+                                          source:source] autorelease];
+  STAssertNotNil(op, nil);
+  
+  [op startQuery];
+  
+  int loops = 0;
+  while (![op isFinished] && loops++ < 20) {
+    // Give the operation 2 seconds to complete
+    [[NSRunLoop currentRunLoop] runUntilDate:
+     [NSDate dateWithTimeIntervalSinceNow:.1]];
+  }
+  STAssertTrue([op isConcurrent], nil);
+  STAssertTrue([op isFinished], nil);
+  STAssertFalse([op isCancelled], nil);
+  
+  STAssertNotNil(results_, nil);
+  
+  PyObject *py = [sharedPython objectForQuery:query
+                          withSearchOperation:nil];
+  STAssertNotNULL(py, nil);
+  
+  NSURL *url = [NSURL URLWithString:@"http://www.google.com/"];
+  NSURL *preview = [NSURL URLWithString:@"file:///icon.png"];
+  NSDictionary *attributes = [NSDictionary dictionaryWithObjectsAndKeys:
+                              @"A Snippet", kHGSObjectAttributeSnippetKey,
+                              preview, kHGSObjectAttributeIconPreviewFileKey,
+                              @"none.test", kHGSObjectAttributeDefaultActionKey,
+                              nil];
+  HGSResult *result = [HGSResult resultWithURL:url
+                                          name:@"Google"
+                                          type:kHGSTypeWebBookmark
+                                        source:source
+                                    attributes:attributes];
+  PyObject *results = PyList_New(1);
+  STAssertNotNULL(results, nil);
+  PyList_SetItem(results, 0, [sharedPython objectForResult:result]);
+  PyObject *setResults = PyString_FromString("SetResults");
+  STAssertNotNULL(setResults, nil);
+  PyObject_CallMethodObjArgs(py, setResults, results, nil);
+  
+  Py_DECREF(results);
+  Py_DECREF(setResults);
+}
+
+- (void)gotResults:(NSNotification *)note {
+  STAssertNotNil([note userInfo], nil);
+  if (results_) {
+    [results_ release];
+  }
+  results_ = [[[note userInfo]
+              valueForKey:kHGSSearchOperationNotificationResultsKey] copy];
+}
+
 @end
