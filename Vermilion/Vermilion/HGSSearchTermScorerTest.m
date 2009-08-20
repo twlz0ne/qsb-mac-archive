cbb6f063396e158f2bde19f26f19f7c3fa4dab9b
qsbmac.daemon <qsbmac.daemon@c7262d1c-4ebb-4cc0-bccf-18c293255191>
2009-08-20 21:30:54 +0000

    [Author: mrossetti]
    Enhance unit testing of term/item scoring algorithm.  See notes in HGSSearchTermScorerTest.m for instructions for constructing the HGSSearchTermScorerTestData.plist file.
    
    PRESUBMIT=passed
    R=dmaclach
    DELTA=145  (144 added, 0 deleted, 1 changed)
    OCL=12400532
    SCL=12411695
    
    
    git-svn-id: file:///Users/gqj/repos/qsb-mac-archive/p/trunk@536 c7262d1c-4ebb-4cc0-bccf-18c293255191

--- Vermilion/Vermilion/HGSSearchTermScorerTest.m
+++ Vermilion/Vermilion/HGSSearchTermScorerTest.m
@@ -33,7 +33,21 @@
 
 #import "GTMSenTestCase.h"
 #import "HGSSearchTermScorer.h"
+#import <Vermilion/HGSBundle.h>
 #import <OCMock/OCMock.h>
+
+// There is an accompanying test data file with the name 
+// HGSSearchTermScorerTestData.plist which is used by the
+// testRelativeTermScoring test case below.  The construct of the test data
+// file is an array of test cases.  Each test case is a dictionary
+// with two items.  One item has a key of 'search terms' and is a string
+// containing one or more words which will be used to score 'search items'.
+// The other item in the dictionary has a key of 'search items' and is
+// an array of strings, each of which will be scored against the 'search
+// terms'.  The array of 'search items' should be in the order in which
+// you expect them to score from highest to lowest.
+
+#define kHGSMaximumRelativeTermScoringTests 100
 
 // We consider any score to be a successful match, but a 'reasonable' score
 // is considered > 5.0, a 'good' score > 10, and an excellent score > 15.
@@ -130,7 +144,7 @@ static const CGFloat kHGSTestPerfectScore = 10000.0;
                score, kHGSTestGoodScore);
 }
 
-- (void)testRelativeTermScoring {
+- (void)testBasicRelativeTermScoring {
   [self ResetScoringFactors];
   CGFloat scoreA = HGSScoreTermForItem(@"abc", @"abcd", nil);
   CGFloat scoreB = HGSScoreTermForItem(@"abc", @"abcde", nil);
@@ -151,6 +165,67 @@ static const CGFloat kHGSTestPerfectScore = 10000.0;
                                nil);
   scoreB = HGSScoreTermForItem(@"can", @"american bandstand of canada", nil);
   STAssertTrue(scoreA > scoreB, @"%f !> %f", scoreA, scoreB);
+}
+
+- (void)testRelativeTermScoring {
+  [self ResetScoringFactors];
+  // Pull in the test data.
+  NSBundle *bundle = HGSGetPluginBundle();
+  STAssertNotNil(bundle, nil);
+  NSString *plistPath = [bundle pathForResource:@"HGSSearchTermScorerTestData"
+                                         ofType:@"plist"];
+  STAssertNotNil(plistPath, nil);
+  NSArray *testList = [NSArray arrayWithContentsOfFile:plistPath];
+  STAssertNotNil(testList, nil);
+  STAssertTrue([testList count] > 0, nil);
+  STAssertTrue([testList count] <= kHGSMaximumRelativeTermScoringTests, nil);
+  
+  // Allow a maximum of kHGSMaximumRelativeTermScoringTests possible
+  // search items.  The scores will be in one-to-one correspondence with the
+  // input testItems and the resulting scores should following a descending
+  // pattern even though the items will be scored randomly.
+  CGFloat itemScores[kHGSMaximumRelativeTermScoringTests];
+  NSUInteger itemIndex[kHGSMaximumRelativeTermScoringTests];  // Used to randomize the items.
+  srandom([NSDate timeIntervalSinceReferenceDate]);
+  
+  for (NSDictionary *test in testList) {
+    for (NSUInteger i = 0; i < kHGSMaximumRelativeTermScoringTests; ++i) {
+      itemScores[i] = 0.0;
+      itemIndex[i] = i;
+    }
+    NSString *testTermsString = [test objectForKey:@"search terms"];
+    NSArray *testTerms = [testTermsString componentsSeparatedByString:@" "];
+    NSArray *testItems = [test objectForKey:@"search items"];
+    NSUInteger itemsCount = [testItems count];
+    for (NSUInteger j = itemsCount; j > 0; --j) {
+      // Pick a random item index then compress the index choices.
+      NSUInteger indexChoice = random() / (LONG_MAX / j);
+      NSUInteger randomIndex = itemIndex[indexChoice];
+      for (NSUInteger k = indexChoice + 1; k < 50; ++k) {
+        itemIndex[k - 1] = itemIndex[k];
+      }
+      NSString *testItem = [testItems objectAtIndex:randomIndex];
+      NSArray *scores = HGSScoreTermsForItem(testTerms, testItem, nil);
+      for (NSNumber *score in scores) {
+        if ([score floatValue] > 0.01) {
+          itemScores[randomIndex] += [score floatValue];
+        } else {
+          // One of the terms didn't match at all so it's not a match.
+          itemScores[randomIndex] = 0.0;
+          break;
+        }
+      }
+    }
+    // Verify that we got ascending scores.
+    for (NSUInteger l = 1; l < itemsCount; ++l) {
+      STAssertTrue(itemScores[l - 1] >= itemScores[l], 
+                   @"Score failure for '%@'[%d]: %0.2f !>= '%@'[%d]: %0.2f "
+                   @"for term '%@'", 
+                   [testItems objectAtIndex:l - 1], l - 1, itemScores[l - 1],
+                   [testItems objectAtIndex:l], l, itemScores[l],
+                   testTermsString);
+    }
+  }
 }
 
 @end
