8fee5460cd8322efe9eb2571f038f8b9c798ff4b
qsbmac.daemon <qsbmac.daemon@c7262d1c-4ebb-4cc0-bccf-18c293255191>
2009-09-29 20:30:39 +0000

    [Author: mrossetti]
    Eliminate header doc error.
    
    PRESUBMIT=passed
    R=dmaclach
    DELTA=5  (5 added, 0 deleted, 0 changed)
    OCL=12942011
    SCL=12942230
    
    
    git-svn-id: file:///Users/gqj/repos/qsb-mac-archive/p/trunk@611 c7262d1c-4ebb-4cc0-bccf-18c293255191

--- Vermilion/Vermilion/HGSCallbackSearchSource.h
+++ Vermilion/Vermilion/HGSCallbackSearchSource.h
@@ -30,6 +30,11 @@
 //  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 //
 
+/*!
+ @header
+ @discussion HGSCallbackSearchSource
+*/
+
 #import "HGSSearchSource.h"
 #import "HGSSimpleArraySearchOperation.h"
 
