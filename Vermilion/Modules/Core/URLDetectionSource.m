6589c2a8dc895d84f8495fc080424c3676d14d86
qsbmac.daemon <qsbmac.daemon@c7262d1c-4ebb-4cc0-bccf-18c293255191>
2010-03-23 19:44:08 +0000

    [Author: dmaclach]
    Tighten up URLDetection source so it gives less false positives,
    and is smarter (slightly) about ranking.
    
    PRESUBMIT=passed
    R=mrossetti
    DELTA=67  (52 added, 3 deleted, 12 changed)
    OCL=15056295
    SCL=15056784
    
    
    git-svn-id: file:///Users/gqj/repos/qsb-mac-archive/p/trunk@952 c7262d1c-4ebb-4cc0-bccf-18c293255191

--- Vermilion/Modules/Core/URLDetectionSource.m
+++ Vermilion/Modules/Core/URLDetectionSource.m
@@ -40,6 +40,37 @@
 // if you are trying to debug something you don't want as a url result, it might
 // not be this source.
 
+// List of top level domains taken from IANA
+// http://data.iana.org/TLD/tlds-alpha-by-domain.txt
+// # Version 2010032300, Last Updated Tue Mar 23 14:07:01 2010 UTC
+
+static const char kUDSIANATLDs[][8] = {
+  "AC", "AD", "AE", "AERO", "AF", "AG", "AI", "AL", "AM", "AN", "AO",
+  "AQ", "AR", "ARPA", "AS", "ASIA", "AT", "AU", "AW", "AX", "AZ", "BA",
+  "BB", "BD", "BE", "BF", "BG", "BH", "BI", "BIZ", "BJ", "BM", "BN", "BO",
+  "BR", "BS", "BT", "BV", "BW", "BY", "BZ", "CA", "CAT", "CC", "CD", "CF",
+  "CG", "CH", "CI", "CK", "CL", "CM", "CN", "CO", "COM", "COOP", "CR",
+  "CU", "CV", "CX", "CY", "CZ", "DE", "DJ", "DK", "DM", "DO", "DZ", "EC",
+  "EDU", "EE", "EG", "ER", "ES", "ET", "EU", "FI", "FJ", "FK", "FM", "FO",
+  "FR", "GA", "GB", "GD", "GE", "GF", "GG", "GH", "GI", "GL", "GM", "GN",
+  "GOV", "GP", "GQ", "GR", "GS", "GT", "GU", "GW", "GY", "HK", "HM", "HN",
+  "HR", "HT", "HU", "ID", "IE", "IL", "IM", "IN", "INFO", "INT", "IO",
+  "IQ", "IR", "IS", "IT", "JE", "JM", "JO", "JOBS", "JP", "KE", "KG",
+  "KH", "KI", "KM", "KN", "KP", "KR", "KW", "KY", "KZ", "LA", "LB", "LC",
+  "LI", "LK", "LR", "LS", "LT", "LU", "LV", "LY", "MA", "MC", "MD", "ME",
+  "MG", "MH", "MIL", "MK", "ML", "MM", "MN", "MO", "MOBI", "MP", "MQ",
+  "MR", "MS", "MT", "MU", "MUSEUM", "MV", "MW", "MX", "MY", "MZ", "NA",
+  "NAME", "NC", "NE", "NET", "NF", "NG", "NI", "NL", "NO", "NP", "NR",
+  "NU", "NZ", "OM", "ORG", "PA", "PE", "PF", "PG", "PH", "PK", "PL", "PM",
+  "PN", "PR", "PRO", "PS", "PT", "PW", "PY", "QA", "RE", "RO", "RS", "RU",
+  "RW", "SA", "SB", "SC", "SD", "SE", "SG", "SH", "SI", "SJ", "SK", "SL",
+  "SM", "SN", "SO", "SR", "ST", "SU", "SV", "SY", "SZ", "TC", "TD", "TEL",
+  "TF", "TG", "TH", "TJ", "TK", "TL", "TM", "TN", "TO", "TP", "TR",
+  "TRAVEL", "TT", "TV", "TW", "TZ", "UA", "UG", "UK", "US", "UY", "UZ",
+  "VA", "VC", "VE", "VG", "VI", "VN", "VU", "WF", "WS", "YE", "YT", "YU",
+  "ZA", "ZM", "ZW"
+};
+
 @interface URLDetectionSource : HGSCallbackSearchSource
 @end
 
@@ -76,43 +107,61 @@
   HGSTokenizedString *tokenizedQueryString = [query tokenizedQueryString];
   NSString *urlString = [tokenizedQueryString originalString];
   NSURL *url = [NSURL URLWithString:urlString];
-
+  CGFloat score = 0;
+  
   if ([url scheme]) {
     // NSURL seem happy, nothing more to do at this point, we'll use it.
+    score = HGSCalibratedScore(kHGSCalibratedStrongScore);
   } else {
     // Try to see if it's "internalsite/bar" or "google.com" style
     NSArray *pathComponents = [urlString componentsSeparatedByString:@"/"];
     NSString *host = [pathComponents objectAtIndex:0];
     NSArray *hostComponents = [host componentsSeparatedByString:@"."];
     
-    BOOL valid = NO;
-
     // IP Address
     if ([hostComponents count] == 4) {
-      valid = YES;
-    }
-    // internalsite/[something]
-    else if ([host length] && [hostComponents count] == 1
-               && [pathComponents count] > 1) { 
-      valid = YES;
-    }
-    // blah.com
-    else if ([hostComponents count] > 1
-             // Dissalow "default.htm*" and moo.228
-             && ![[hostComponents lastObject] hasPrefix:@"htm"]
-             && [[hostComponents lastObject] rangeOfCharacterFromSet:
-                 [NSCharacterSet decimalDigitCharacterSet]].location == NSNotFound){
-      valid = YES;
+      BOOL isNumber = YES;
+      NSCharacterSet *decimalDigits = [NSCharacterSet decimalDigitCharacterSet];
+      NSCharacterSet *nonDigits = [decimalDigits invertedSet];
+      for (NSString *component in hostComponents) {
+        if ([component rangeOfCharacterFromSet:nonDigits].length != 0) {
+          isNumber = NO;
+          break;
+        }
+      }
+      if (isNumber) {
+        score = HGSCalibratedScore(kHGSCalibratedStrongScore);
+      }
     }
     
-    if (valid) {
+    // internalsite/[something]
+    if ((!(score > 0)) && [host length] && [hostComponents count] == 1
+        && [pathComponents count] > 1) {
+      score = HGSCalibratedScore(kHGSCalibratedWeakScore);
+    }
+    // blah.com
+    if ((!(score > 0)) && [hostComponents count] > 1) {
+      NSString *lastComponent = [hostComponents lastObject];
+      const char *cLastComponent = [[lastComponent uppercaseString] UTF8String];
+      size_t lastLength = strlen(cLastComponent);
+      for (size_t i = 0; 
+           i < sizeof(kUDSIANATLDs) / sizeof(kUDSIANATLDs[0]); 
+           ++i) {
+        if (strncasecmp(cLastComponent, kUDSIANATLDs[i], lastLength) == 0) {
+          score = HGSCalibratedScore(kHGSCalibratedStrongScore);
+          size_t ianaLength = strlen(kUDSIANATLDs[i]);
+          score *= (((float)lastLength) / ((float)ianaLength));
+        }
+      }
+    }
+    
+    if (score > 0) {
       urlString = [@"http://" stringByAppendingString:urlString];
       url = [NSURL URLWithString:urlString];
     }
   }
 
-  if (url) {
-    CGFloat score = HGSCalibratedScore(kHGSCalibratedStrongScore);
+  if (score > 0) {
     NSDictionary *attributes
       = [NSDictionary dictionaryWithObjectsAndKeys:
          [NSImage imageNamed:@"blue-nav"], kHGSObjectAttributeIconKey,
